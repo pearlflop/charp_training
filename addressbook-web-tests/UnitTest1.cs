eef85559ea8fa5113e85b4b551502ba87b6a26ee
pearlflop <pearlflop@gmail.com>
2025-04-06 23:38:48 +1000

    Project created

---
+++ addressbook-web-tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace addressbook_web_tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
