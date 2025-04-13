20e5913974e551ec011a4c341f5bd352d4e2fa2b
pearlflop <pearlflop@gmail.com>
2025-04-14 01:43:23 +1000

    task 4

---
+++ addressbook-web-tests/ContactData.cs
@@ -0,0 +1,25 @@
+namespace addressbook_web_tests;
+
+public class ContactData
+{
+    private string firstName;
+    private string lastName;
+
+    public ContactData(string firstName, string lastName)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public string FirstName
+    {
+        get { return firstName; }
+        set { firstName = value; }
+    }
+
+    public string LastName
+    {
+        get { return lastName; }
+        set { lastName = value; }
+    }
+}
